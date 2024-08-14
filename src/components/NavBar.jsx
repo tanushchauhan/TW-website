ebf4bd2fe676d68cdb7c201b3609b30ba42f7b65
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-08-13 20:15:51 -0500

    Added more content to the NavBar.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -66,7 +66,92 @@ function NavBar() {
     },
   };
 
-  return <div></div>;
+  const navigate = useNavigate();
+
+  return (
+    <header className="text-gray-900 dark:text-gray-200 flex text-xl justify-between items-center px-4 py-5 lg:px-8 lg:py-10 z-20">
+      <img
+        src={Logo}
+        alt="logo"
+        className="h-16 lg:h-20 cursor-pointer"
+        onClick={() => navigate("/")}
+      />
+      <div className="lg:flex gap-8 hidden">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="about">About</NavLink>
+        <NavLink to="contact">Contact Us</NavLink>
+      </div>
+      <div className="flex gap-5">
+        <button className="modeBtn" onClick={handleClick}>
+          {!currMode ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-10 lg:w-16 dark:stroke-gray-200 stroke-gray-800"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-10 lg:w-16 dark:stroke-gray-200 stroke-gray-800"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+              />
+            </svg>
+          )}
+        </button>
+        <button
+          className="menuBtn z-10 dark:stroke-gray-200 stroke-gray-800 lg:hidden"
+          aria-expanded="false"
+          onClick={handleMenuClick}
+        >
+          <svg
+            stroke="var(--button-color)"
+            className="hamburger w-12"
+            viewBox="0 0 100 100"
+          >
+            <line
+              className="line top"
+              x1="90"
+              x2="10"
+              y1="40"
+              y2="40"
+              strokeWidth="10"
+              strokeLinecap="round"
+              strokeDasharray="80"
+              strokeDashoffset="0"
+            ></line>
+            <line
+              className="line bottom"
+              x1="10"
+              x2="90"
+              y1="60"
+              y2="60"
+              strokeWidth="10"
+              strokeLinecap="round"
+              strokeDasharray="80"
+              strokeDashoffset="0"
+            ></line>
+          </svg>
+        </button>
+      </div>
+    </header>
+  );
 }
 
 export default NavBar;
